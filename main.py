12f8a2208254091f3444d6e5bad05d13af3a5f1b
Jules <143351368+JulesElo@users.noreply.github.com>
2026-06-17 10:09:17 -0300

    feat: update main.py menu to include alternative k-means and manual convolution scripts

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
+from src import ex1_kmeans_clustering
 from src import ex1_quantization
 from src import ex2_subtraction
 from src import ex3_high_boost
+from src import ex4_convolution_manual
 from src import ex4_convolution
 
 def main():
@@ -9,32 +11,40 @@ def main():
         print("\n" + "="*50)
         print("    MENU: TAREFA AVALIATIVA DE VISÃO COMPUTACIONAL    ")
         print("="*50)
-        print("[1] Exercício 1 - Transformação de Tons (Quantização)")
-        print("[2] Exercício 2 - Subtração de Fundo e Detecção")
-        print("[3] Exercício 3 - Filtro High-Boost")
-        print("[4] Exercício 4 - Teorema da Convolução")
+        print("[1] Exercício 1 - Quantização Uniforme")
+        print("[2] Exercício 1 - Clusterização K-Means")
+        print("[3] Exercício 2 - Subtração de Fundo e Detecção")
+        print("[4] Exercício 3 - Filtro High-Boost")
+        print("[5] Exercício 4 - Teorema da Convolução")
+        print("[6] Exercício 4 - Convolução Manual")
         print("[0] Sair")
         print("-" * 50)
         
         escolha = input("Escolha uma opção para executar: ")
         
         if escolha == '1':
-            print("\nExecutando Exercício 1...")
+            print("\nExecutando Exercício 1 - Quantização Uniforme...")
             ex1_quantization.run()
         elif escolha == '2':
+            print("\nExecutando Exercício 1 - Clusterização K-Means...")
+            ex1_kmeans_clustering.run()
+        elif escolha == '3':
             print("\nExecutando Exercício 2...")
             ex2_subtraction.run()
-        elif escolha == '3':
+        elif escolha == '4':
             print("\nExecutando Exercício 3...")
             ex3_high_boost.run()
-        elif escolha == '4':
-            print("\nExecutando Exercício 4...")
+        elif escolha == '5':
+            print("\nExecutando Exercício 4 - Teorema da Convolução...")
             ex4_convolution.run()
+        elif escolha == '6':
+            print("\nExecutando Exercício 4 - Convolução Manual...")
+            ex4_convolution_manual.run()
         elif escolha == '0':
-            print("\nEncerrando o programa. Até logo!")
+            print("\nEncerrando o programa...")
             sys.exit(0)
         else:
-            print("\nOpção inválida! Por favor, digite um número de 0 a 4.")
+            print("\nOpção inválida! Por favor, digite um número de 0 a 6.")
 
 if __name__ == "__main__":
     main()
